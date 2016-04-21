ffcd947c6fa53f7fb38f212889bb1d21b40823d1
shaybix <shaybix@tuta.io>
2016-04-21 23:56:33 +0100

    added a couple more methods, and comments

--- lexer.go
+++ lexer.go
@@ -1,13 +1,19 @@
 package ankaboot
 
+// Greatly inspired by Rob Pike's talk on Lexical Analysis in Go
+// and borrowed a lot of the code from slide, to get it to work.
+// Will require to modify the code to be more suitable for my own
+// use case.
+//
 type itemType int
 
+// Define all of the 'items' the lexer will need to lex.
 const (
 	itemError itemType = iota // Error
 	itemEOF                   // End Of File
 	itemLTag                  // Left Tag <
 	itemRTag                  // Right Tag >
-	itemHTML
+	itemHTML                  // HTML tag
 	itemHead
 	itemLink
 	itemTitle
@@ -27,11 +33,49 @@ const (
 	itemHref
 )
 
+// item struct that will be passed through lexer items channel to the
+// parser.
 type item struct {
 	typ itemType
 	val string
 }
 
-func lexItem() {
+// lexer struct
+type lexer struct {
+	name  string
+	input string
+	start int
+	pos   int
+	width int
+	items chan item
+}
 
+// lex initialises the lexer
+func lex(name, input string) (*lexer, chan item) {
+	l := &lexer{
+		name:  name,
+		input: input,
+		items: make(chan item),
+	}
+
+	go l.run()
+	return l, l.items
+}
+
+// run lexer
+func (l *lexer) run() {
+	// here will run
+	close(l.items)
+}
+
+// emit tokens
+func (l *lexer) emit(t itemType) {
+	l.items <- item{t, l.input[l.start:l.pos]}
+	l.start = l.pos
+
+}
+
+//close open channel
+func close(items chan item) {
+	// close channel
 }
