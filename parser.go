a8900bcc8aed62ec708918576fd499b7f711f946
shaybix <shaybix@tuta.io>
2016-04-23 13:01:40 +0100

    layed out the functionality of crawl, and inited parse funciton in parser.go

---
+++ parser.go
@@ -0,0 +1,6 @@
+package ankaboot
+
+func parse(body []byte) ([]string, error) {
+	//
+	return nil, nil
+}
--- spider.go
+++ spider.go
@@ -1,7 +1,14 @@
 package ankaboot
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 type Target struct {
 	Title   string
+	url     string
 	Targets []string
 }
 
@@ -11,6 +18,31 @@ type Spider struct {
 }
 
 // Crawl ...
-func (s *Spider) Crawl(url string) []*Element {
-	return nil
+func (s *Spider) Crawl(url string) []*Target {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	links, err := parse(body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var targets []*Target
+	for _, link := range links {
+
+		target := new(Target)
+		target.url = link
+		targets = append(targets, target)
+
+	}
+
+	return targets
 }
